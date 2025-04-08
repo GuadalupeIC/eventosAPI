d884a4f104ef16dccd288a041c985b402bfe06d8
GuadalupeIC <lupisirineo@gmail.com>
2025-04-07 21:24:23 -0600

    se cambia scripts del package.json

--- src/app.js
+++ src/app.js
@@ -2,15 +2,11 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import eventRoutes from './routes/event.routes.js';
-import clientRoutes from './routes/client.routes.js';
 import helmet from "helmet";
-import { connectMongoDB } from './mongo.config.js';
 
 const app = express();
 
 app.use(express.json());
-
-await connectMongoDB();
 
 app.use(
     helmet({
